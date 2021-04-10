8041cd02cce3b5dbc92a7fd8b5bada41a05b3809
2lifetop <57310468+2lifetop@users.noreply.github.com>
2021-04-10 15:26:19 +0800

    Update webhook.php

--- api/webhook.php
+++ api/webhook.php
@@ -7,8 +7,8 @@ $data = json_decode(file_get_contents("php://input"), true);
 $message = $data['message']['text'] ?? '';
 if ($message === '/start') {
     $chat_id = $data['message']['chat']['id'];
-    $startMessage = "**关于：** tg酱（@realtgchat_bot）是一个简单易用的TG消息发送机器人，只要是有网络的地方，你就可以通过浏览器输入一串网址即可让TG酱向您的TG发送通知------------ 第一次使用请发送 /token 获取个人专属token ------------**使用方法：** ```https://tgbot-red.vercel.app/api?token=TG获取的token&message=你要发送的消息```"
-    $bot->sendMessage(['text' => $startMessage, 'chat_id' => $chat_id], "parse_mode"=> markdown);
+    $startMessage = "<br>**关于：**</br> tg酱（@realtgchat_bot）是一个简单易用的TG消息发送机器人，只要是有网络的地方，你就可以通过浏览器输入一串网址即可让TG酱向您的TG发送通知------------ 第一次使用请发送 /token 获取个人专属token ------------**使用方法：** ```https://tgbot-red.vercel.app/api?token=TG获取的token&message=你要发送的消息```"
+    $bot->sendMessage(['text' => $startMessage, 'chat_id' => $chat_id], "parse_mode"=> html);
 }
 if ($message === '/token') {
     $chat_id = $data['message']['chat']['id'];
